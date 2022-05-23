07e7a1ef9b8dac5cfa412c62d5616fe04e48197d
Bruce Tang <brucetang6@gmail.com>
2022-05-23 13:48:49 -0400

    proxy issue

--- server1.js
+++ server1.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.use((request,response,next) => {
-    console.log('有人请求服务器1了');
+    console.log('有人请求服务器 1 了');
     next()
 })
 
@@ -16,5 +16,5 @@ app.get('/students', (request,response) => {
 })
 
 app.listen(5000,(err)=>{
-    if(!err) console.log('服务器1启动成功了， 请求学生信息地址为： http://localhost:5000/students');
+    if(!err) console.log('服务器1启动成功了, 请求学生信息地址为： http://localhost:5000/students');
 })
