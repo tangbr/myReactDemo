d1184325dc28eac758f2338f4e50d6f52a62c151
Bruce Tang <brucetang6@gmail.com>
2022-05-23 00:47:02 -0400

    aa

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class App extends Component {
+
+  getStudentData = () => {
+    axios.get('http://localhost:5000/students').then(
+      response => {console.log('成功',response.data);},
+      error => {console.log('失败了',error);} 
+    ) 
+  }
+
   render() {
     return (
-      <div>App.......</div>
+      <div>
+        <button onClick={this.getStudentData}>点我获取学生数据</button>
+
+      </div>
     )
   }
 }
