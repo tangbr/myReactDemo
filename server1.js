27718f3451199537fd2a9488f3d8127628f07ed3
Bruce Tang <brucetang6@gmail.com>
2022-05-23 15:57:33 -0400

    a

--- server1.js
+++ server1.js
@@ -3,6 +3,8 @@ const app = express()
 
 app.use((request,response,next) => {
     console.log('有人请求服务器 1 了');
+    console.log('请求来自于',request.get('Host'));
+    console.log('请求的地址是',request.url);
     next()
 })
 
@@ -16,5 +18,5 @@ app.get('/students', (request,response) => {
 })
 
 app.listen(5000,(err)=>{
-    if(!err) console.log('服务器1启动成功了, 请求学生信息地址为： http://localhost:5000/students');
+    if(!err) console.log('服务器 1 启动成功了, 请求学生信息地址为： http://localhost:5000/students');
 })
