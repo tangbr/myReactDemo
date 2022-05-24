b6b573a906ccac07683b18db13aec49510213116
Bruce Tang <brucetang6@gmail.com>
2022-05-23 23:26:16 -0400

    aa

---
+++ src/components/List/index.jsx
@@ -0,0 +1,41 @@
+import React, { Component } from 'react'
+import './index.css'
+
+export default class List extends Component {
+    render() {
+       return (
+            <div className="row">
+                <div className="card">
+           <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+             <img alt="head_portrait" src="https://img1.baidu.com/it/u=3916367244,2765862625&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=500" style={{width: '100px'}} />
+           </a>
+           <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+           <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+             <img alt="head_portrait" src="https://img2.baidu.com/it/u=457681302,3981348939&fm=253&fmt=auto&app=138&f=JPEG?w=352&h=288" style={{width: '100px'}} />
+           </a>
+           <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+           <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+             <img alt="head_portrait" src="https://img2.baidu.com/it/u=1141669830,3812989879&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=471" style={{width: '100px'}} />
+           </a>
+           <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+           <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+             <img alt="head_portrait" src="https://img2.baidu.com/it/u=3612926555,522546903&fm=253&fmt=auto&app=138&f=JPEG?w=200&h=200" style={{width: '100px'}} />
+           </a>
+           <p className="card-text">reactjs</p>
+                </div>
+                <div className="card">
+           <a rel="noreferrer" href="https://github.com/reactjs" target="_blank">
+             <img alt="head_portrait" src="https://img1.baidu.com/it/u=1847922946,915956888&fm=253&fmt=auto&app=138&f=JPEG?w=343&h=400" style={{width: '100px'}} />
+           </a>
+           <p className="card-text">reactjs</p>
+                </div>
+            </div>
+       )
+    }
+}
